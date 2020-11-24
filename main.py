2984ccdb813655afe6fc349acfcb9f4cc5b00e47
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2020-11-24 12:42:03 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print('Welcome to the MineCraft CommandConsole, Press /help for a list of commands.')
+
+while True:
+    x = input('/')
+    if x=='lol':
+        print('lol')
+    else:
+        print('Unknown Command ' + x)
